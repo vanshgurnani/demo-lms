b747a44ef80ffb85e730fddd917f7e7e54c1a44a
Vansh Gurnani <gurnanivansh57@gmail.com>
2024-01-26 20:27:36 +0530

    Revert "done corrs"
    This reverts commit 967aedfc32fe6a507ad00a71118190d6d8f0be22.

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,7 @@ const cors = require('cors');
 
 app.use(cors({
   origin: ['http://localhost:3000',
-           'https://demo-lms-q1or.vercel.app',
-          'https://demo-lms-36b7.vercel.app'],
+           'https://demo-lms-q1or.vercel.app'],
            // Specify the allowed origin
   methods: ['GET', 'DELETE','PUT','POST'], // Specify the allowed HTTP methods as an array
   // You can include other options as needed
